7d3aef0cd32b5b32f3c067a17082b2da2668e409
vedh kumar <vedhkumar237@gmail.com>
2025-07-31 10:47:51 +0530

    stake

--- programs/nft-staking/src/instructions/unstake.rs
+++ programs/nft-staking/src/instructions/unstake.rs
@@ -1,9 +1,7 @@
 use anchor_lang::prelude::*;
 use anchor_spl::{
     metadata::{
-        mpl_token_metadata::instructions::{
-            FreezeDelegatedAccountCpi, FreezeDelegatedAccountCpiAccounts,
-        },
+        mpl_token_metadata::{accounts::MasterEdition, types::Collection},
         MasterEditionAccount, Metadata, MetadataAccount,
     },
     token::{approve, Approve, Mint, Token, TokenAccount},
@@ -15,7 +13,7 @@ use crate::{
 };
 
 #[derive(Accounts)]
-pub struct Stake<'info> {
+pub struct Unstake<'info> {
     #[account(mut)]
     pub user: Signer<'info>,
 
@@ -73,53 +71,8 @@ pub struct Stake<'info> {
     pub system_program: Program<'info, System>,
 }
 
-impl<'info> Stake<'info> {
-    pub fn process_stake(&mut self, bumps: &StakeBumps) -> Result<()> {
-        require!(
-            self.user_account.amount_staked < self.stake_config.max_stake,
-            ErrorCode::MaxStakeExceeded
-        );
-
-        self.stake_account.set_inner(StakeAccount {
-            owner: self.user.key(),
-            mint: self.mint.key(),
-            staked_at: Clock::get().unwrap().unix_timestamp,
-            bump: bumps.stake_account,
-        });
-        // The program has to freeze the mint ATA
-        approve(
-            CpiContext::new(
-                self.token_program.to_account_info(),
-                Approve {
-                    authority: self.user.to_account_info(),
-                    to: self.mint_ata.to_account_info(), // we are approving the mint ATA to the stake account
-                    delegate: self.stake_account.to_account_info(),
-                },
-            ),
-            1,
-        )?;
-
-        let signers_seeds: &[&[&[u8]]] = &[&[
-            STAKE_ACCOUNT_SEED.as_bytes(),
-            &self.mint.key().as_ref(),
-            &self.stake_config.key().as_ref(),
-            &[bumps.stake_account],
-        ]];
-
-        FreezeDelegatedAccountCpi::new(
-            &self.metadata_token_program.to_account_info(),
-            FreezeDelegatedAccountCpiAccounts {
-                delegate: &self.stake_account.to_account_info(),
-                edition: &self.master_edition.to_account_info(),
-                mint: &self.mint.to_account_info(),
-                token_program: &self.token_program.to_account_info(),
-                token_account: &self.mint_ata.to_account_info(),
-            },
-        )
-        .invoke_signed(signers_seeds)?;
-
-        self.user_account.amount_staked += 1;
-
+impl<'info> Unstake<'info> {
+    pub fn process_stake(&mut self) -> Result<()> {
         Ok(())
     }
 }
